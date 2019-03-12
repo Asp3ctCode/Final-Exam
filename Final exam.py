e9523f9e99eb6d07d21dcd7e399b3ba9fccf76c7
Tyler Goodrich <02tyler.goodrich@gulllakecs.org>
2019-03-12 09:22:06 -0700

    Final Exam 1

--- Final exam.py
+++ Final exam.py
@@ -1,1 +1,4 @@
-print ("Hello I did it")
+budget = int(input("What is your budget?"))
+if budget >= 450 and budget < 500:
+    print("Processer: Intel i3  GPU: Gtx 980  Ram: 8gb ballistix ddr4 Case: Thermaltake CA-1B3-00M1NN-00 Versa")
+    

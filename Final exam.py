a188476ae814949aec7e8bbf0465a772194e5ddf
Tyler Goodrich <02tyler.goodrich@gulllakecs.org>
2019-03-13 09:27:43 -0700

    Final Exam 1.2

--- Final exam.py
+++ Final exam.py
@@ -1,4 +1,10 @@
+#Tyler Goodrich
+#3/13/19
+#This program will take your budget and it will give you the specs of what pc you should build.
+
 budget = int(input("What is your budget?"))
 if budget >= 450 and budget < 500:
-    print("Processer: Intel i3  GPU: Gtx 980  Ram: 8gb ballistix ddr4 Case: Thermaltake CA-1B3-00M1NN-00 Versa")
-    
+    print("Processer: Intel i3  GPU: Gtx 980  Ram: 4gb ballistix ddr4 Case: Thermaltake CA-1B3-00M1NN-00 Versa")
+budget = int(input("What is your budget?"))
+if budget >= 550 and budget < 600:
+    print("Processer: Intel i5 GPU: Gtx 980  Ram: 4gb ballistix ddr4 Case: ")
